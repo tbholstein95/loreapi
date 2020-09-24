579dff9608de0f3b9332234bf7d7181d94cd114e
tbholstein95 <60832007+tbholstein95@users.noreply.github.com>
2020-09-24 12:10:27 -0400

    rework views for response objects

--- loreapi/lore/views.py
+++ loreapi/lore/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from lore.models import Lore
 from lore.serializers import LoreSerializer
 
 
 # Create your views here.
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def lore_list(request):
 	"""
 
@@ -16,15 +16,14 @@ def lore_list(request):
 	if request.method == 'GET':
 		lore = Lore.objects.all()
 		serializer = LoreSerializer(lore, many=True)
-		return JsonResponse(serializer.data, safe=False)
+		return Response(serializer.data)
 
 	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = LoreSerializer(data=data)
+		serializer = LoreSerializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data, status=201)
-		return JsonResponse(serializer.errors, status=400)
+			return Response(serializer.data, status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 def character_entry_detail(request, pk):
