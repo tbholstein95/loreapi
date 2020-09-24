97d617748b90f6627e342141f3162246c601bbb1
tbholstein95 <60832007+tbholstein95@users.noreply.github.com>
2020-09-24 08:52:57 -0400

    Add Lore Model

--- loreapi/lore/models.py
+++ loreapi/lore/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Lore(models.Model):
+	created = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=100, blank=True, default='')
+	firstname = models.TextField()
+
+	# Orders models by when they were created
+	class Meta:
+		ordering = ['created']
+
+
+# python manage.py makemigrations snippets
+# python manage.py migrate
